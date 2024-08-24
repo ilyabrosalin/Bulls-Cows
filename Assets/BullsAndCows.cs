4b683e2e49df5c670b470a230710d45b5b5d9b0a
ilyabrosalin <brosalinio@gmail.com>
2024-08-24 23:20:22 +0300

    refactoring and add listeners in script

---
+++ Assets/BullsAndCows.cs
@@ -0,0 +1,14 @@
+﻿public struct BullsAndCows
+{
+    int bulls;
+    int cows;
+
+    public BullsAndCows(int bulls, int cows)
+    {
+        this.bulls = bulls;
+        this.cows = cows;
+    }
+
+    public int Cows { get => cows; set => cows = value; }
+    public int Bulls { get => bulls; set => bulls = value; }
+}
--- Assets/main.cs
+++ Assets/main.cs
@@ -1,57 +1,63 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.SceneManagement;
-
 
 public class Main : MonoBehaviour
 {
-    public Main()
+    [SerializeField] private GameObject _inputField;
+    [SerializeField] private GameObject _rulesPanel;
+    [SerializeField] private GameObject _makeAMoveBtn;
+    [SerializeField] private GameObject _newGameBtn;
+    [SerializeField] private Button _showRulesButton;
+    [SerializeField] private Button _hideRulesButton;
+    [SerializeField] private Text _output;
+    [SerializeField] private Text _strong;
+    [SerializeField] private Text _alert;
+
+    private string _hiddenNumber;
+    
+    private void Start()
     {
-        hiddenNumber = GetRandomNumber();
+        _hiddenNumber = GetRandomNumber();
+        _showRulesButton.onClick.AddListener(() => _rulesPanel.SetActive(true));
+        _hideRulesButton.onClick.AddListener(() => _rulesPanel.SetActive(false));
+        _makeAMoveBtn.GetComponent<Button>().onClick.AddListener(OnClickMakeAMoveBtn);
+        _newGameBtn.GetComponent<Button>().onClick.AddListener(OnClickNewGameBtn);
     }
 
-    public GameObject inputField;
-    public GameObject makeAMoveBtn;
-    public GameObject newGameBtn;
-    public Text output;
-    public Text strong;
-    public Text alert;
-
-    public static string hiddenNumber;
-
-    public string GetRandomNumber()
+    private string GetRandomNumber()
     {
-        System.Random rnd = new System.Random();
         string result;
         do
         {
-            result = (rnd.Next(1000, 9999)).ToString();
+            result = UnityEngine.Random.Range(1000, 9999).ToString();
         }
         while (!OnlyUniqueDigits(result));
         return result;
     }
 
-    public Tuple<int, int> SearchBullsAndCows(string hiddenNumber, string userNumber)
+    private BullsAndCows SearchBullsAndCows(string hiddenNumber, string userNumber)
     {
-        int bulls = 0;
-        int cows = 0;
-        for (int i = 0; i < 4; i++)
+        var bulls = 0;
+        var cows = 0;
+        for (var i = 0; i < 4; i++)
         {
-            if (hiddenNumber.Contains(userNumber[i]))
+            if (!hiddenNumber.Contains(userNumber[i]))
             {
-                if (hiddenNumber[i] == userNumber[i])
-                    bulls++;
-                else
-                    cows++;
+                continue;
+            }
+            if (hiddenNumber[i] == userNumber[i])
+            {
+                bulls++;
+            }
+            else
+            {
+                cows++;
             }
         }
-        return Tuple.Create(bulls,cows);
+        return new BullsAndCows(bulls,cows);
     }
 
-    static bool OnlyUniqueDigits(string number)
+    private bool OnlyUniqueDigits(string number)
     {
         for (int i = 0, n = number.Length; i < n - 1; i++)
         {
@@ -66,82 +72,95 @@ public class Main : MonoBehaviour
         return true;
     }
 
-    public void OnClickNewGameBtn()
+    private void ChangeAlert(string text, Color color)
     {
-        output.text = "";
-        strong.text = "";
-        alert.text = "";
-        alert.color = Color.black;
-        makeAMoveBtn.SetActive(true);
-        inputField.SetActive(true);
-        newGameBtn.SetActive(false);
+        _alert.text = text;
+        _alert.color = color;
     }
 
-    public void OnClickMakeAMoveBtn()
+    private void ClearAlert()
     {
-        Debug.Log(hiddenNumber);
-        alert.text = "";
-        alert.color = Color.black;
-        string userNumber = inputField.GetComponent<InputField>().text;
+        ChangeAlert("", Color.black);
+    }
+
+    private void OnClickNewGameBtn()
+    {
+        ClearAlert();
+        _output.text = "";
+        _strong.text = "";
+        _makeAMoveBtn.SetActive(true);
+        _inputField.SetActive(true);
+        _newGameBtn.SetActive(false);
+    }
+
+    private void OnClickMakeAMoveBtn()
+    {
+        Debug.Log(_hiddenNumber);
+        ClearAlert();
+        var userNumber = _inputField.GetComponent<InputField>().text;
         var isNumeric = int.TryParse(userNumber, out _);
         var isUnique = OnlyUniqueDigits(userNumber);
-        
-        if (isNumeric && isUnique && userNumber.Length==4)
-        {
-            (int bulls, int cows) = (SearchBullsAndCows(hiddenNumber, userNumber).Item1, SearchBullsAndCows(hiddenNumber, userNumber).Item2);
-            string bullsDesc = "";
-            string cowsDesc = "";
-            switch (bulls)
-            {
-                case 0:
-                    bullsDesc = " быков ";
-                    break;
-                case 1:
-                    bullsDesc = " бык ";
-                    break;
-                case 2 or 3 or 4:
-                    bullsDesc = " быка ";
-                    break;
-            }
-            switch (cows)
-            {
-                case 0:
-                    cowsDesc = " коров ";
-                    break;
-                case 1:
-                    cowsDesc = " корова ";
-                    break;
-                case 2 or 3 or 4:
-                    cowsDesc = " коровы ";
-                    break;
-            }
-            string outText = inputField.GetComponent<InputField>().text + ": " + bulls + bullsDesc + cows + cowsDesc + "\n";
-            if (bulls + cows >= 3) 
-                strong.text = strong.text.Insert(0, outText);
-            else
-                output.text = output.text.Insert(0, outText);
 
-            inputField.GetComponent<InputField>().text = "";
-            if (bulls == 4)
-            {
-                alert.text = "Мууу! Победа!";
-                alert.color = Color.green;
-                makeAMoveBtn.SetActive(false);
-                inputField.SetActive(false);
-                newGameBtn.SetActive(true);
-                hiddenNumber = GetRandomNumber();
-            }
+        if (!isUnique)
+        {
+            ChangeAlert("Цифры не должны повторяться", Color.red);
+            return;
+        }
+
+        if (!isNumeric)
+        {
+            ChangeAlert("Код должен быть числом", Color.red);
+            return;
+        }
+
+        if (userNumber.Length != 4)
+        {
+            ChangeAlert("Код - четырехзначное число", Color.red);
+            return;
+        }
+
+        var bullsAndCows = SearchBullsAndCows(_hiddenNumber, userNumber);
+        var bullsDesc = bullsAndCows.Bulls switch
+        {
+            0 => " быков ",
+            1 => " бык ",
+            2 or 3 or 4 => " быка ",
+            _ => "error"
+        };
+
+        var cowsDesc = bullsAndCows.Cows switch
+        {
+            0 => " коров ",
+            1 => " корова ",
+            2 or 3 or 4 => " коровы ",
+            _ => "error"
+        };
+        
+        var outText = userNumber + ": " + bullsAndCows.Bulls + bullsDesc + bullsAndCows.Cows + cowsDesc + "\n";
+
+        if (bullsAndCows.Bulls + bullsAndCows.Cows >= 3)
+        {
+            _strong.text = _strong.text.Insert(0, outText);
         }
         else
         {
-            alert.color = Color.red;
-            if (!isUnique) alert.text = "Цифры не должны повторяться";
-            if (!isNumeric || userNumber.Length != 4) alert.text = "Код - четырехзначное число";
+            _output.text = _output.text.Insert(0, outText);
+        }
+
+        _inputField.GetComponent<InputField>().text = "";
+
+        if (bullsAndCows.Bulls == 4)
+        {
+            Win();   
         }
     }
 
-    public void GoToRules()
+    private void Win()
     {
-        SceneManager.LoadScene("Rules");
+        ChangeAlert("Мууу! Победа!", Color.green);
+        _makeAMoveBtn.SetActive(false);
+        _inputField.SetActive(false);
+        _newGameBtn.SetActive(true);
+        _hiddenNumber = GetRandomNumber();
     }
 }
